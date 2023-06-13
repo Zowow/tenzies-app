ca3b27e2101003315aa835611fe322baa9a4f335
zowow <79232193+zowow@users.noreply.github.com>
2023-06-14 04:50:31 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -2,43 +2,127 @@ import React from "react";
 import "./style.css"
 import Die from "./components/Die"
 import {nanoid} from "nanoid"
+import Confetti from "react-confetti";
 
 function App() {
 
   //functions
+  const startTimer = () => {
+    setRunning(true);
+  }
+
+  const stopTimer = () => {
+    setMinutes(0);
+    setSeconds(0);
+    setRunning(false);
+  }
+
+  const generateNewDice = () => {
+    return {
+      value: Math.ceil(Math.random() * 6),
+      isHeld: false,
+      id: nanoid()
+    }
+  }
+
   const allNewDice = () => {
     const newDices = []
     for(let i = 0; i < 10; i++){
-      newDices[i] = ({
-        value: Math.ceil(Math.random() * 6),
-        isHeld: false,
-        id: nanoid()
-      })
+      newDices[i] = (generateNewDice())
     }
     return newDices
   }
 
   const rerollDice = () => {
-    setDices(allNewDice())
+    if(!tenzies){
+      setDices(prevDice => {
+        return prevDice.map(item => {
+          return item.isHeld === true ? item : generateNewDice()
+        })
+      })
+      setNumRolls(prevRoll => prevRoll + 1)
+    }else{
+      setTenzies(false)
+      setDices(allNewDice())
+      setNumRolls(0)
+      startTimer()
+    }
   }
 
+  const holdDice = (id) => {
+    setDices(prevDice => {
+      return prevDice.map((item)=> {
+        return item.id === id ? {...item, isHeld: !item.isHeld} : item
+      })
+    })
+  }
+  
   //end of functions
-  const [dices, setDices] = React.useState(allNewDice())
 
+  const [dices, setDices] = React.useState(allNewDice())
+  const [tenzies, setTenzies] = React.useState(false)
+  const [numRolls, setNumRolls] = React.useState(0)
+  const [minutes, setMinutes] = React.useState(0);
+  const [seconds, setSeconds] = React.useState(0);
+  const [running, setRunning] = React.useState(true);
+
+  React.useEffect(() => {
+    let intervalId;
+
+    if (running) {
+      intervalId = setInterval(() => {
+        if (seconds < 59) {
+          setSeconds((prevSeconds) => prevSeconds + 1);
+        } else {
+          setSeconds(0);
+          setMinutes((prevMinutes) => prevMinutes + 1);
+        }
+      }, 1000);
+    }
+
+    return () => clearInterval(intervalId);
+  }, [running, seconds]);
+  
+
+  React.useEffect(()=> {
+    const allHeld = dices.every(item => item.isHeld)
+    const firstVal = dices[0].value
+    const allSameValue = dices.every(item => item.value === firstVal)
+    if(allHeld && allSameValue){
+      setTenzies(true)
+      console.log("You Won!")
+      stopTimer()
+    }
+  },[dices])
+
+  
   const dicesMap = dices.map(item => {
-    return (<Die value={item.value} isHeld={item.isHeld} key={item.id} />)
+    return (<Die 
+                dice={item} 
+                key={item.id} 
+                holdDice={() => holdDice(item.id)} 
+            />)
   })
 
   return (
     <main>
       <div className="header">
+        {tenzies && <Confetti height={550} width={550}/>}
         <h1>Tenzies</h1>
         <h3>Roll until all dice are the same. Click each die to freeze it at its current value between rolls.</h3>
+        <div className="dice-setup">
+          <span>Timer : {minutes < 10 ? `0${minutes}` : minutes}:{seconds < 10 ? `0${seconds}` : seconds}</span>
+          <span>Number of Rolls : {numRolls}</span>
+        </div>
       </div>
       <div className="dice-container">
         {dicesMap}
       </div>
-      <button onClick={rerollDice} className="roll">Roll</button>
+      <button 
+        onClick={rerollDice} 
+        className="roll">
+        {tenzies ? "New Game" : "Roll"}
+      </button>
     </main>
   );
 }
--- src/components/Die.js
+++ src/components/Die.js
@@ -2,13 +2,98 @@ import React from "react";
 
 const Die = (props) => {
 
-    const styles = {
-        background: props.isHeld ? "#59E391" : "white"
+    const diceStyles = {
+        background: props.dice.isHeld ? "#59E391" : "white",
     }
 
+    const dotStyles = {
+        backgroundColor: props.dice.isHeld ? "white" : "black"
+    }
+
+    // Dice Face
+    const diceOne = (value) => {
+        if(value === 1){
+            return (
+                <div class="dice first-face" >
+                    <span class="dot" style={dotStyles}></span>
+                </div>
+            )
+        }else if (value === 2){
+            return (
+                <div class="dice second-face">
+                    <span class="dot" style={dotStyles}> </span>
+                    <span class="dot" style={dotStyles}> </span>
+                </div>
+            )
+        }else if (value === 3){
+            return(
+            <div class="dice third-face">
+                <span class="dot" style={dotStyles}></span>
+                <span class="dot" style={dotStyles}></span>
+                <span class="dot" style={dotStyles}></span>
+            </div>
+            )
+        }else if (value === 4) {
+            return (
+            <div class="dice fourth-face">
+                <div class="column">
+                    <span class="dot" style={dotStyles}></span>
+                    <span class="dot" style={dotStyles}></span>
+                </div>
+                <div class="column">
+                    <span class="dot" style={dotStyles}></span>
+                    <span class="dot" style={dotStyles}></span>
+                </div>
+            </div>
+            )
+        }else if (value === 5) {
+            return (
+                <div class="fifth-face dice">
+                    <div class="column">
+                        <span class="dot" style={dotStyles}></span>
+                        <span class="dot" style={dotStyles}></span>
+                    </div>
+                    
+                    <div class="column">
+                        <span class="dot" style={dotStyles}></span>
+                    </div>
+                    
+                    <div class="column">
+                        <span class="dot" style={dotStyles}></span>
+                        <span class="dot" style={dotStyles}></span>
+                    </div>
+                </div>
+            )
+        }else{
+            return(
+                <div class="fourth-face dice">
+                    <div class="column">
+                        <span class="dot" style={dotStyles}></span>
+                        <span class="dot" style={dotStyles}></span>
+                        <span class="dot" style={dotStyles}></span>
+                    </div>
+                    <div class="column">
+                        <span class="dot" style={dotStyles}></span>
+                        <span class="dot" style={dotStyles}></span>
+                        <span class="dot" style={dotStyles}></span>
+                    </div>
+                </div>
+            )
+        }
+
+        
+    }
+
+    // end of Dice Face
+
     return (
-        <div className="dice" style={styles}>
-            <h2 className="dice-num" >{props.value}</h2>
+        
+        <div 
+            onClick={props.holdDice} 
+            className="dices" 
+            style={diceStyles}
+        >
+            {diceOne(props.dice.value)}
         </div>
     )
 }
